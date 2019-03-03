217e57e760e01c9e65d5d8bc1e231a6a5c6438c1
Jennifer Wilson <jenniferwilson@Sunshine.local>
2019-03-02 19:40:35 -0700

    Mars homework.

--- Week12/app.py
+++ Week12/app.py
@@ -1,29 +1,23 @@
-# Dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import pymongo
-import datetime
 import scrape_mars
 
 app = Flask(__name__)
 
-#Use flask_pymongo to set up mongo connection
+# Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
 mongo = PyMongo(app)
 
+@app.route("/")
+def index():
+    results = mongo.db.mars_data.find_one()
+    return render_template("index.html", results=results)
+
 @app.route("/scrape")
-def scrape():
-    # Drops collection if available.
-    mars.collection.remove()
-    # https://stackoverflow.com/questions/45553629/how-to-run-a-python-script-on-flask-backend
-    scrape_mars.scrape()
-
-# # create route that renders index.html template
-# @app.route("/")
-# def index():
-
-#     return render_template("index.html", dogs=dogs)
-
+def scraper():
+   results = scrape_mars.master_scrape()
+   mongo.db.mars_data.update({},results, upsert=True)
+   return redirect("/",302)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Week12/scrape_mars.py
+++ Week12/scrape_mars.py
@@ -1,33 +1,33 @@
-# Import packages.
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import requests
 import json
-import pymongo
+import re
 
-# The default port used by MongoDB is 27017
-# https://docs.mongodb.com/manual/reference/default-mongodb-port/
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
+results_dict = {}
 
-# Declare the database
-db = client.mars
-
-# Declare the collection
-collection = db.mars
-
-# Get NASA Mars News and store results as a list of dictionaries.
-
+def master_scrape():
+    print("Starting scrape.")
+    
+    # Get data which stores all data in dictionary above.
+    get_featured_image()
+    get_articles()
+    get_facts()
+    get_hemisphere_images()
+    get_mars_weather()
+    print("Finished scraping!")
+    return results_dict
+    
 def get_articles():
-    news = {}
+    news = []
     
     news_url = "https://mars.nasa.gov/api/v1/news_items/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     resp = requests.get(news_url, headers=header)
     resp = resp.json()
 
-    # Get articls
+    # Get articles
     articles = resp.get('items')
     
     # Iterate over all elements in articles to extract title and body.
@@ -39,27 +39,27 @@ def get_articles():
         body = articles[i]['body'].encode('utf-8')
         body = re.sub(r'<.*?>', '', body)
         
-        news['article_'+ str(i)] = {
+        news.append({
             "title": title,
             "body": body
-        }
+        })
         
-    collection.insert_one(news)
+    results_dict['news'] = news
 
 def get_featured_image():
-    featured_image = {}
-    image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"    
-    executable_path = {'executable_path': '/Users/jenniferwilson/Desktop/Repositories/UDEN201811DATA3-Homework/chromedriver'}
-    browser = Browser('chrome', **executable_path)
+    # Browser initialized within master_scrape function. 
+    image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"   
+    executable_path = {"executable_path": "/Users/jenniferwilson/Desktop/Repositories/UDEN201811DATA3-Homework/Week12/chromedriver"}
+    browser = Browser('chrome', **executable_path) 
     browser.visit(image_url)
     html = browser.html
     soup = bs(html, 'html.parser')
     featured_image_url = "https://www.jpl.nasa.gov" + soup.article["style"].split("'")[1].encode('utf-8')
-    featured_image['featured_image_url'] = featured_image_url
-    collection.insert_one(featured_image)
+    
+    # Store in summary dictionary.
+    results_dict['featured_image'] = featured_image_url
 
 def get_mars_weather():
-    weather = {}
     twitter_url = "https://twitter.com/marswxreport?lang=en"
     header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     resp = requests.get(twitter_url, headers=header)
@@ -70,25 +70,28 @@ def get_mars_weather():
 
     # Remove ASCII characters.
     tweet_text = str(tweet_text.encode("ascii","ignore"))
-    weather['weather'] = tweet_text
-    collection.insert_one(weather)
-
-image_page_urls = []
-
-def get_hemisphere_urls():
-    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    resp = requests.get(url, headers=header)
-    soup = bs(resp.text,'html.parser')
-    hrefs = soup.find_all("a", {"class": "itemLink product-item"})
     
-    for i in range(len(hrefs)):
-        href_trimmed = hrefs[i]['href'].encode('utf-8')
-        image_page_urls.append("https://astrogeology.usgs.gov/" + href_trimmed)
+    # Store in summary dictionary.
+    results_dict['weather'] = tweet_text
 
-def get_hemisphere_image():
+def get_hemisphere_images():
     
-    hemisphere_image_urls = {}
+    image_page_urls = []
+
+    def get_hemisphere_urls():
+        url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+        header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        resp = requests.get(url, headers=header)
+        soup = bs(resp.text,'html.parser')
+        hrefs = soup.find_all("a", {"class": "itemLink product-item"})
+
+        for i in range(len(hrefs)):
+            href_trimmed = hrefs[i]['href'].encode('utf-8')
+            image_page_urls.append("https://astrogeology.usgs.gov/" + href_trimmed)
+    
+    get_hemisphere_urls()
+    
+    images = []
     
     for i in range(len(image_page_urls)):
         url = image_page_urls[i]        
@@ -97,20 +100,23 @@ def get_hemisphere_image():
         soup = bs(resp.text,'html.parser')
         
         image_url = soup.find_all("a", attrs={"target": "_blank"})
-        image_url = image_url[1]["href"].encode('utf-8')
+        image_url = image_url[0]["href"].encode('utf-8')
         title = soup.find('h2', {"class":"title"}).get_text().encode('utf-8')
         
-        hemisphere_image_urls['image_'+ str(i)] = {
+        images.append({
             "img_url": image_url,
             "title": title
-        }
+        })
         
-    collection.insert_one(hemisphere_image_urls)
+    results_dict['hemisphere_images'] = images
 
-def scrape():
-    get_articles()
-    get_featured_image()
-    get_mars_weather()
-    get_hemisphere_urls()
-    get_hemisphere_image()
-    #get_facts()
+def get_facts():
+    facts_df = {}
+    facts_url = "http://space-facts.com/mars/"
+    facts = pd.read_html(facts_url)
+    facts_df = facts[0]
+    facts_df.rename(columns={0: "Stat", 1:"Values"}, inplace = True) 
+    facts_df['Values'] = map(lambda x: x.encode('ascii', 'ignore').decode('ascii'), facts_df["Values"])
+    facts_html = facts_df.to_html()
+    results_dict['facts'] = facts_html
+
