af37c5e99528397ed9dbdbecb2b0835d4945fd50
Jennifer Wilson <jenniferwilson@Sunshine.local>
2019-03-12 20:16:54 -0600

    Finished Week 14 homework.

---
+++ Practice/Bar Chart and Filtering Arrays/plots.js
@@ -0,0 +1,44 @@
+var eyeColor = ["Brown", "Brown", "Brown", "Brown", "Brown",
+  "Brown", "Brown", "Brown", "Green", "Green",
+  "Green", "Green", "Green", "Blue", "Blue",
+  "Blue", "Blue", "Blue", "Blue"];
+var eyeFlicker = [26.8, 27.9, 23.7, 25, 26.3, 24.8,
+  25.7, 24.5, 26.4, 24.2, 28, 26.9,
+  29.1, 25.7, 27.2, 29.9, 28.5, 29.4, 28.3];
+
+// @TODO: Complete the Following Steps
+
+// Create the Trace
+var trace =
+  {
+      x: eyeColor,
+      y: eyeFlicker,
+      type: 'bar'
+  };
+
+let brownVals = eyeColor.map((value, index, array) => {
+    // console.log(val)
+    if (value == "Brown"){
+      return eyeFlicker[index];
+    }
+  })
+
+let brownValsTwo = [];
+  eyeColor.forEach((value, index, array) => {
+    if(val == "Brown") {
+      brownValsTwo.push(eyeFlicker[index])
+    }
+  })
+// Create the data array for our plot
+var data = [trace];
+
+// Define our plot layout
+var layout = {
+  title: "Eye Color vs. Flicker",
+  xaxis: {title: "Eye Color"},
+  yaxis: {title: "Eye Flicker"}
+}
+
+
+// Plot the chart to a div tag with id "bar-plot"
+Plotly.newPlot('bar-plot', data, layout);
--- Week 14/static/js/app.js
+++ Week 14/static/js/app.js
@@ -1,44 +1,46 @@
+// render all data upon loading window (used in function below to skip filtering data)
+window.onload = function(e){ 
+    renderUfoTable('ignoreFiltering')
+}
 
 // attach a listener to the input field
 let myButton = document.getElementById("filter-btn")
-
-// Commented out during debugging.
-// myButton.addEventListener("click", function(e) {
-//     x = document.getElementById('datetime').value
-//     return renderUfoTable(x)
-// })
-
-// Identify the input field which is what will be 
-// used to filter the data.
-let myInput = document.getElementById("datetime");
 
 // Add a listener to the button and set it to pull 
 // in the value from the input field myInput.
 // Take the date from myInput and render the table.
 myButton.addEventListener("click", function(e) {
+    // since the form is attempting to submit to the page itself, 
+    // use e.preventDefault() immediately in the callback function
+    e.preventDefault()
+
+    // Pulled within the listener since we want a fresh value every time.
+    let myInput = document.getElementById("datetime");
+
+    // Added to check that it is working.
     console.log(myInput.value)
+
     return renderUfoTable(myInput.value)
 })
 
-// Commented out during debugging.
-// d3.select("#filter-btn").on("click",function(){
-//     myInput = d3.select("#datetime").event.target.value
-//     console.log(myInput)
-//     debugger
-// })
-
 function renderUfoTable(inputDate) {
-    // from data.js
+
     var tableData = data;
+
+    // clear any data in the table
+    document.getElementById("ufo-table-body").innerHTML = "";
 
     // identify the table in the HTML
     let myTableBody = document.getElementById('ufo-table-body');
 
+    if (inputDate == 'ignoreFiltering') {
+        filteredTableData = data;// th whole thing
+    }   else {
+        // filter bsaed on ur form
+        filteredTableData = tableData.filter(x => x.datetime == inputDate)
+    }
     // filter the data for only the date provided in the input field
-    let filteredTableData = tableData.filter(row => {
-        return row.datetime = inputDate;
-        }
-    )
+    
     
     // using the filtered dataset, render the table rows
     filteredTableData.forEach(items => {
