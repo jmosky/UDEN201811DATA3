1f2fe799dc1561a42f7e43f7e3a3db20f2772e34
Jennifer Wilson <jenniferwilson@Sunshine.local>
2019-04-09 21:06:30 -0600

    Adding plotly chart.

--- Project_2/static/js/plots.js
+++ Project_2/static/js/plots.js
@@ -1,28 +1,31 @@
-// var trace1 = {
-//     x: ['giraffes', 'orangutans', 'monkeys'],
-//     y: [20, 14, 23],
-//     name: 'SF Zoo',
-//     type: 'bar'
-//   };
-  
-// var trace2 = {
-//     x: ['giraffes', 'orangutans', 'monkeys'],
-//     y: [12, 18, 29],
-//     name: 'LA Zoo',
-//     type: 'bar'
-//   };
-  
-//   var data = [trace1, trace2];
-  
-//   var layout = {barmode: 'stack'};
-  
-//   Plotly.newPlot('plot', data, layout);
-
 var url = "http://127.0.0.1:5000/data/osmp_lands"
+d3.json(url).then(function(response) {
 
-d3.json(url, function(error, data) {
-  if (error) return console.warn(error);
-  var layout = {barmode: 'group'};
+  let yesPublicAccess = response.filter(data => {
+    return data.publicAccess === "y";
+  })
+
+  let noPublicAccess = response.filter(data => {
+    return data.publicAccess === "n";
+  })
   
- Plotly.newPlot('plot', data.data, layout);
+  var trace1 = {
+    x: noPublicAccess.map(data => data.Manager), 
+    y: noPublicAccess.map(data => data.sumAcres),
+    name: 'Public Access: No',
+    type: 'bar'
+  };
+
+  var trace2 = {
+    x: yesPublicAccess.map(data => data.Manager),
+    y: yesPublicAccess.map(data => data.sumAcres), 
+    name: 'Public Access: Yes', 
+    type: 'bar'
+  };
+
+  var data = [trace1, trace2];
+
+  var layout = {barmode: 'stack'};
+
+  Plotly.newPlot('plot', data, layout);
 });
