39f0eedafb21b9892b6055824534778ec728a023
Jennifer Wilson <jenniferwilson@Sunshine.local>
2019-04-09 20:34:10 -0600

    Got plotly to see JSON data.

--- Project_2/static/js/plots.js
+++ Project_2/static/js/plots.js
@@ -1,19 +1,28 @@
-var trace1 = {
-    x: ['giraffes', 'orangutans', 'monkeys'],
-    y: [20, 14, 23],
-    name: 'SF Zoo',
-    type: 'bar'
-  };
+// var trace1 = {
+//     x: ['giraffes', 'orangutans', 'monkeys'],
+//     y: [20, 14, 23],
+//     name: 'SF Zoo',
+//     type: 'bar'
+//   };
   
-  var trace2 = {
-    x: ['giraffes', 'orangutans', 'monkeys'],
-    y: [12, 18, 29],
-    name: 'LA Zoo',
-    type: 'bar'
-  };
+// var trace2 = {
+//     x: ['giraffes', 'orangutans', 'monkeys'],
+//     y: [12, 18, 29],
+//     name: 'LA Zoo',
+//     type: 'bar'
+//   };
   
-  var data = [trace1, trace2];
+//   var data = [trace1, trace2];
   
-  var layout = {barmode: 'stack'};
+//   var layout = {barmode: 'stack'};
   
-  Plotly.newPlot('plot', data, layout);
+//   Plotly.newPlot('plot', data, layout);
+
+var url = "http://127.0.0.1:5000/data/osmp_lands"
+
+d3.json(url, function(error, data) {
+  if (error) return console.warn(error);
+  var layout = {barmode: 'group'};
+  
+ Plotly.newPlot('plot', data.data, layout);
+});
